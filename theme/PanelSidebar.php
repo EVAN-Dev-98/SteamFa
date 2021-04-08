b0384d92bc61854484769d73dc24d7c96c184d2a
Sajad Gholami <76253073+EVAN-Dev-98@users.noreply.github.com>
2021-04-08 17:34:25 +0430

    - in panelsidebar.php rename Manager to manager bug fixed

--- theme/PanelSidebar.php
+++ theme/PanelSidebar.php
@@ -1,6 +1,6 @@
 <aside class="panel-sidebar" id="aside">
     <ul>
-        <li class="items"><h1><a href="<?php if (isset($path)) echo $path; ?>Manager/">پنل مدیریت</a></h1></li>
-        <li class="items"><a href="<?php if (isset($path)) echo $path; ?>Manager/AddProduct.php">افزودن محصول</a></li>
+        <li class="items"><h1><a href="<?php if (isset($path)) echo $path; ?>manager/">پنل مدیریت</a></h1></li>
+        <li class="items"><a href="<?php if (isset($path)) echo $path; ?>manager/AddProduct.php">افزودن محصول</a></li>
     </ul>
 </aside>
