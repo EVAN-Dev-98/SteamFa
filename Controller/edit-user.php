a36b80b9d71950f1bb879e7f191496078c2042fc
Sajad Gholami <76253073+EVAN-Dev-98@users.noreply.github.com>
2021-05-01 13:59:48 +0430

    - add edit product - add edit user - Optimization panel

--- Controller/edit-user.php
+++ Controller/edit-user.php
@@ -11,7 +11,7 @@ if ( isset($_GET['id']) ){
         $parameters['id'] = $_GET['id'];
         unset($parameters['submit']);
         Users::update( $parameters );
-        redirect("../View/account/profile-user.php");
+        redirect("../View/account/show-users.php");
     }
 
     $table = Users::find("ID = {$_GET['id']}");
