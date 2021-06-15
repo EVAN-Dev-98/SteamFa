7ec1b672e2b0de8bba9de8b437c4f57c3b940656
Sajad Gholami <76253073+EVAN-Dev-98@users.noreply.github.com>
2021-06-15 18:20:23 +0430

    - catalog products bug fixed and improve design

--- Controller/get-item.php
+++ Controller/get-item.php
@@ -9,17 +9,15 @@ $p_all = array_merge($p_csgo,$p_dota2);
 $alerts = Alert::alerts();
 ?>
 <section class="container mt-5">
-    <?php if ($alerts) { ?>
-        <section> <?php echo $alerts; ?> </section>
-    <?php } ?>
+    <?php if ($alerts) { ?><section> <?php echo $alerts; ?> </section><?php } ?>
     <section class="row">
-<?php
-$count = 0;
-foreach ($p_all as $row){
-    get_template_part("product","template",$row);
-    $count++;
-}
-?>
+        <?php
+        $count = 0;
+        foreach ($p_all as $row){
+            get_template_part("product","template",$row);
+            $count++;
+        }
+        ?>
     </section>
 <?php if ($count == 0 ){ ?> <h3 class="text-center">متاسفانه محصولی برای نمایش وجود ندارد</h3> <?php } ?>
 </section>
--- View/template/product-template.php
+++ View/template/product-template.php
@@ -2,28 +2,38 @@
 if ($attr_name == "attr_dota2"){
     ?>
     <section class="col-lg-3 col-md-4 col-sm-6 my-3">
-        <article class="card shadow-sm">
-            <img src="<?php echo $img; ?>" class="card-img-top" alt="">
+        <article class="card shadow-sm p-card">
+            <a href="<?php echo view("product-detail.php?id={$id}"); ?>" class="p-img" style="background-image: url(<?php echo $img; ?>)"></a>
             <section class="card-body">
                 <h4 class="card-title text-center"><a href="<?php echo view("product-detail.php?id={$id}"); ?>" target="_steamfa_product"><?php echo $name; ?></a></h4>
                 <p class="card-text">بازی : دوتا 2</p>
-                <p class="card-text">هیرو : <?php echo $hero_id; ?></p>
-                <p class="card-text">سطح نادر بودن : <?php echo $rarity; ?></p>
-                <p class="card-text">کیفیت : <?php echo $quality; ?></p>
+                <section class="p-price mb-2">
+                    <p>قیمت :<span class="p-price-n mx-2">12500</span>تومان</p>
+                </section>
+                <a href="#" class="btn btn-outline-primary">افزودن به سبد خرید</a>
             </section>
-            <section class="card-footer">
-                <?php
-                if ( $owner_id == Authentication::uid() ) {
-                    if ( Authorization::check("ItemDelete") ) { ?><a href="<?php echo controller("do-delete-item.php?self={$id}"); ?>" class="mx-1 btn btn-outline-danger">حذف</a><?php }
-                    if ( Authorization::check("ItemEdit") ) { ?><a href="<?php echo account("edit-item.php?self={$id}"); ?>" class="mx-1 btn btn-outline-warning">ویرایش</a><?php }
-                }
-                else {
-                    if ( Authorization::check("ItemDeleteOther") ) { ?><a href="<?php echo controller("do-delete-item.php?id={$id}"); ?>" class="mx-1 btn btn-outline-danger">حذف</a><?php }
-                    if ( Authorization::check("ItemEditOther") ) { ?><a href="<?php echo account("edit-item.php?id={$id}"); ?>" class="mx-1 btn btn-outline-warning">ویرایش</a><?php }
-                }
-                ?>
-                <!--a href="" class="btn btn-primary">افزودن به سبد خرید</a-->
-            </section>
+            <?php if ( Authentication::check() ) { ?>
+                <?php if ( $owner_id == Authentication::uid() ) { ?>
+                    <section class="card-footer">
+                        <?php if (Authorization::check("ItemDelete")) { ?>
+                            <a href="#" class="btn btn-outline-danger mx-2">حذف</a>
+                        <?php } ?>
+                        <?php if (Authorization::check("ItemEdit")) { ?>
+                            <a href="#" class="btn btn-outline-warning mx-2">ویرایش</a>
+                        <?php } ?>
+                    </section>
+                <?php }
+                elseif (Authorization::check("ItemDeleteOther") || Authorization::check("ItemEditOther")) { ?>
+                    <section class="card-footer">
+                        <?php
+                        if (Authorization::check("ItemDeleteOther")){ ?>
+                            <a href="#" class="btn btn-outline-danger mx-2">حذف</a>
+                        <?php }
+                        if (Authorization::check("ItemEditOther")){ ?>
+                            <a href="#" class="btn btn-outline-warning mx-2">ویرایش</a>
+                        <?php } ?>
+                    </section>
+                <?php } } ?>
         </article>
     </section>
 <?php
@@ -31,29 +41,38 @@ if ($attr_name == "attr_dota2"){
 if ($attr_name == "attr_csgo"){
     ?>
     <section class="col-lg-3 col-md-4 col-sm-6 my-3">
-        <article class="card shadow-sm">
-            <img src="<?php echo $img; ?>" class="card-img-top" alt="">
+        <article class="card shadow-sm p-card">
+            <a href="<?php echo view("product-detail.php?id={$id}"); ?>" class="p-img" style="background-image: url(<?php echo $img; ?>)"></a>
             <section class="card-body">
                 <h4 class="card-title text-center"><a href="<?php echo view("product-detail.php?id={$id}"); ?>" target="_steamfa_product"><?php echo $name; ?></a></h4>
                 <p class="card-text">بازی : کانتر</p>
-                <p class="card-text">نوع : <?php echo $type; ?></p>
-                <p class="card-text">اسلحه : <?php echo $weapon; ?></p>
-                <p class="card-text">کیفیت : <?php echo $quality; ?></p>
-                <p class="card-text">دسته : <?php echo $category; ?></p>
+                <section class="p-price mb-2">
+                    <p>قیمت :<span class="p-price-n mx-2">12500</span>تومان</p>
+                </section>
+                <a href="#" class="btn btn-outline-primary">افزودن به سبد خرید</a>
             </section>
-            <section class="card-footer">
-                <?php
-                if ( $owner_id == Authentication::uid() ) {
-                    if ( Authorization::check("ItemDelete") ) { ?><a href="<?php echo controller("do-delete-item.php?self={$id}"); ?>" class="mx-1 btn btn-outline-danger">حذف</a><?php }
-                    if ( Authorization::check("ItemEdit") ) { ?><a href="<?php echo account("edit-item.php?self={$id}"); ?>" class="mx-1 btn btn-outline-warning">ویرایش</a><?php }
-                }
-                else {
-                    if ( Authorization::check("ItemDeleteOther") ) { ?><a href="<?php echo controller("do-delete-item.php?id={$id}"); ?>" class="mx-1 btn btn-outline-danger">حذف</a><?php }
-                    if ( Authorization::check("ItemEditOther") ) { ?><a href="<?php echo account("edit-item.php?id={$id}"); ?>" class="mx-1 btn btn-outline-warning">ویرایش</a><?php }
-                }
-                ?>
-                <!--a href="" class="btn btn-primary">افزودن به سبد خرید</a-->
-            </section>
+            <?php if ( Authentication::check() ) { ?>
+                <?php if ( $owner_id == Authentication::uid() ) { ?>
+                    <section class="card-footer">
+                        <?php if (Authorization::check("ItemDelete")) { ?>
+                            <a href="#" class="btn btn-outline-danger mx-2">حذف</a>
+                        <?php } ?>
+                        <?php if (Authorization::check("ItemEdit")) { ?>
+                            <a href="#" class="btn btn-outline-warning mx-2">ویرایش</a>
+                        <?php } ?>
+                    </section>
+                <?php }
+                elseif (Authorization::check("ItemDeleteOther") || Authorization::check("ItemEditOther")) { ?>
+                        <section class="card-footer">
+                            <?php
+                                if (Authorization::check("ItemDeleteOther")){ ?>
+                                    <a href="#" class="btn btn-outline-danger mx-2">حذف</a>
+                                <?php }
+                                if (Authorization::check("ItemEditOther")){ ?>
+                                    <a href="#" class="btn btn-outline-warning mx-2">ویرایش</a>
+                                <?php } ?>
+                        </section>
+                <?php } } ?>
         </article>
     </section>
 <?php
