6679689788181fd2f51a4801d1085f2e9a66f823
Sajad Gholami <76253073+EVAN-Dev-98@users.noreply.github.com>
2021-06-21 01:54:34 +0430

    - add delete-role

---
+++ Controller/do-delete-role.php
@@ -0,0 +1,28 @@
+<?php
+/* Created By Evan ( Sajad Gholami ) */
+include "__php__.php";
+
+if (!Authorization::check("RoleMaster")){
+    Alert::alerts("متاسفانه شما مجوز حذف مجوز را ندارید!");
+    redirect(account("dashboard.php"));
+}
+
+if (isset($_GET['id'])){
+    $id = $_GET['id'];
+    if (is_numeric($id) && $id > 0){
+        $db = new DB();
+        $table = Role::find("id = {$id}");
+        if (isset($table[0])){
+            Role::delete( $id );
+            Alert::alerts("مجوز مورد نظر با موفقیت حذف شد.",null,"success");
+        }
+        else
+            Alert::alerts("مجوزی با این شناسه وجود ندارد!");
+        unset($db);
+    }
+    else
+        Alert::alerts("شناسه نامعتبر!");
+}
+else
+    Alert::alerts("شناسه دریافت نشد!");
+redirect(account("role-manager.php"));
